5f962f6dd5eeb818a54d6438d0883d987377525c
khushboop-optimus <khushboo.punia@optimusinfo.com>
2013-09-17 18:05:35 +0530

    android assignment

---
+++ android_assign1/src/com/example/android_assign1/exlist.java
@@ -0,0 +1,15 @@
+package com.example.android_assign1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class exlist extends Fragment{
+	
+	 public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		  return inflater.inflate(R.layout.exlist, container, false);
+		 }
+
+}
